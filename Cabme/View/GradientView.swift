7470453372e573ccc6b3100d689c33c85814969a
Surinder Pal Singh Sidhu <surindersidhu16@gmail.com>
2019-08-18 09:42:49 -0400

    added gradient layer on home screen

---
+++ Cabme/View/GradientView.swift
@@ -0,0 +1,28 @@
+//
+//  GradientView.swift
+//  Cabme
+//
+//  Created by surinder pal singh sidhu on 2019-08-08.
+//  Copyright © 2019 surinder. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setupGradientView()
+    }
+    
+    func setupGradientView() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+
+}
